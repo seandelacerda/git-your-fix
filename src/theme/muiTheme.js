edc66dacded2cfb2111523e2a2ad1099e3cd9976
Sean DeLacerda <seanpdlc@gmail.com>
2019-07-29 22:23:07 -0700

    create material ui theme file

---
+++ src/theme/muiTheme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const muiTheme = createMuiTheme({
+    typography: {
+        fontFamily: ['proxima-nova'].join(','),
+    },
+    palette: {
+        primary: {
+            main: '#4568F9',
+        },
+        secondary: {
+            main: '#9E1CB5',
+        },
+    },
+});
+
+export default muiTheme;
