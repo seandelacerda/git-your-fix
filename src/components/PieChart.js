14d1c21255156b69185e5bb2e76abe96d6be3096
Sean DeLacerda <seanpdlc@gmail.com>
2019-08-02 23:11:35 -0700

    use chartjs for pie chart

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,101 +1,54 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import {fade, makeStyles, withStyles} from '@material-ui/core/styles';
+import createChartParams from '../utils/createChartParams';
 import PropTypes from 'prop-types';
-import * as d3 from 'd3';
+import PolarAreaChart from 'react-chartjs-2';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     root: {
-        padding: 24,
-        display: 'flex',
-        justifyContent: 'center',
-    },
-    svg: {
-        width: 300,
-        height: 300,
-    },
-    g: {
-        transform: 'translate(50%, 50%)',
-    },
-});
 
-const colors = ['#9f15b6', '#add24c', '#36c9d4'];
-const radiuses = [1, 0.5, 0.75];
+    },
+}));
 
-class PieChart extends React.PureComponent {
-    static propTypes = {
-        classes: PropTypes.object.isRequired,
-        label: PropTypes.string.isRequired,
-        data: PropTypes.array.isRequired,
+const PieChart = ({ langData, repoData }) => {
+    const classes = useStyles();
+    console.log(langData)
+    const [langChartData, setLangChartData] = useState(null);
+    const initLangChart = () => {
+        const labels = langData.map(lang => lang.label);
+        const data = langData.map(lang => lang.value);
+
+        setLangChartData(data);
+
+        if (data.length > 0) {
+            const backgroundColor = langData.map(
+                ({ color }) => `#${color.length > 4 ? color.slice(1) : color.slice(1).repeat(2)}B3`,
+            );
+            const borderColor = langData.map(lang => `${lang.color}`);
+            const config = { labels, data, backgroundColor, borderColor };
+            setLangChartData(createChartParams(config));
+        }
     };
 
-    componentDidMount() {
-        const { classes, label: chartLabel, data } = this.props;
-        const radius = 120;
-        const pie = d3
-            .pie()
-            .sort(d => d.index)
-            .padAngle(0.005 * Math.PI)
-            .value(d => d.value)(data);
-        const outerRadius = d => radius * (0.5 + radiuses[d.index] * 0.5);
-        const arc = d3
-            .arc()
-            .outerRadius(outerRadius)
-            .innerRadius(radius * 0.5);
-        const svg = d3.select(`.${classes.g}`);
-        const g = svg
-            .selectAll()
-            .data(pie)
-            .enter()
-            .append('g');
-        g.append('path')
-            .attr('d', arc)
-            .style('fill', d => colors[d.index]);
-        const labelArc = d3
-            .arc()
-            .outerRadius(outerRadius)
-            .innerRadius(outerRadius);
-        const label = g
-            .append('g')
-            .attr('transform', d => `translate(${labelArc.centroid(d)})`);
-        label
-            .append('circle')
-            .attr('r', 20)
-            .attr('stroke', d => colors[d.index])
-            .attr('stroke-width', 2)
-            .attr('fill', '#fff');
-        label
-            .append('text')
-            .attr('text-anchor', 'middle')
-            .attr('alignment-baseline', 'central')
-            .text(d => d.data.label)
-            .style('fill', '#646B80');
-        svg
-            .append('foreignObject')
-            .attr('width', 100)
-            .attr('height', 60)
-            .style('transform', 'translate(-50px, -30px)')
-            .append('xhtml:div')
-            .style('display', 'flex')
-            .style('align-items', 'center')
-            .style('justify-content', 'center')
-            .style('height', '100%')
-            .style('font-size', '18px')
-            .style('color', '#646B80')
-            .style('font-weight', 600)
-            .style('text-align', 'center')
-            .text(chartLabel);
-    }
+    useEffect(() => {
+        if (langData.length) {
+            initLangChart();
+        }
+    }, []);
 
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className={classes.root}>
-                <svg className={classes.svg}>
-                    <g className={classes.g} />
-                </svg>
-            </div>
-        );
-    }
-}
+    const chartSize = 300;
+    const langChartError = !(langChartData && langChartData.length > 0);
 
-export default withStyles(styles)(PieChart);
+    return (
+        <div className={classes.root}>
+            <PolarAreaChart data={langChartData}/>
+        </div>
+    );
+};
+
+PieChart.propTypes = {
+    langData: PropTypes.array.isRequired,
+    repoData: PropTypes.array.isRequired,
+};
+
+export default PieChart;
